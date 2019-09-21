3923acd4241cc0fe57aa19c5f0509a6d79f8fd78
Jose Quezada <quezada007@yahoo.com>
2019-09-21 11:13:08 -0700

    update Board component

--- src/js/components/Board.jsx
+++ src/js/components/Board.jsx
@@ -2,27 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Square from './Square';
 
-const Board = ({ handleClick }) => (
+const Board = ({ squares, handleClick }) => (
     <div className="board">
         <div className="board__row">
-            <Square value="0" handleClick={handleClick} />
-            <Square value="1" handleClick={handleClick} />
-            <Square value="2" handleClick={handleClick} />
+            <Square value={squares[0]} handleClick={() => handleClick(0)} />
+            <Square value={squares[1]} handleClick={() => handleClick(1)} />
+            <Square value={squares[2]} handleClick={() => handleClick(2)} />
         </div>
         <div className="board__row">
-            <Square value="3" handleClick={handleClick} />
-            <Square value="4" handleClick={handleClick} />
-            <Square value="6" handleClick={handleClick} />
+            <Square value={squares[3]} handleClick={() => handleClick(3)} />
+            <Square value={squares[4]} handleClick={() => handleClick(4)} />
+            <Square value={squares[5]} handleClick={() => handleClick(5)} />
         </div>
         <div className="board__row">
-            <Square value="7" handleClick={handleClick} />
-            <Square value="8" handleClick={handleClick} />
-            <Square value="9" handleClick={handleClick} />
+            <Square value={squares[6]} handleClick={() => handleClick(6)} />
+            <Square value={squares[7]} handleClick={() => handleClick(7)} />
+            <Square value={squares[8]} handleClick={() => handleClick(8)} />
         </div>
     </div>
 );
 
 Board.propTypes = {
+    squares: PropTypes.arrayOf(
+        PropTypes.string
+    ).isRequired,
     handleClick: PropTypes.func.isRequired
 };
 
