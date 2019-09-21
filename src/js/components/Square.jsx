7e38bf485e7393c6997cc4b50d737124a5ecd9dd
Jose Quezada <quezada007@yahoo.com>
2019-09-21 11:12:40 -0700

    update Square component

--- src/js/components/Square.jsx
+++ src/js/components/Square.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Square = ({ value, handleClick }) => <button type="button" className="board__square" onClick={() => handleClick(value)}>{value}</button>;
+const Square = ({ value, handleClick }) => <button type="button" className="board__square" aria-label={value === null ? 'Empty' : value} onClick={() => handleClick(value)}>{value}</button>;
+
+Square.defaultProps = {
+    value: ''
+};
 
 Square.propTypes = {
-    value: PropTypes.string.isRequired,
+    value: PropTypes.string,
     handleClick: PropTypes.func.isRequired
 };
 
