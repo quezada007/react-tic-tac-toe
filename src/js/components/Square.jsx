8419471a11f38c35dee227f85846e4b9605b1a46
Jose Quezada <quezada007@yahoo.com>
2019-09-21 12:32:20 -0700

    update class name in square component

--- src/js/components/Square.jsx
+++ src/js/components/Square.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Square = ({ value, handleClick }) => <button type="button" className="board__square" aria-label={value === null ? 'Empty' : value} onClick={() => handleClick(value)}>{value}</button>;
+const Square = ({ value, handleClick }) => <button type="button" className="game__board__square" aria-label={value === null ? 'Empty' : value} onClick={() => handleClick(value)}>{value}</button>;
 
 Square.defaultProps = {
     value: ''
