c0e0f7741e66217b155c6bad49ea931dfac28205
Jose Quezada <quezada007@yahoo.com>
2019-09-21 12:32:36 -0700

    update classes in board component

--- src/js/components/Board.jsx
+++ src/js/components/Board.jsx
@@ -3,18 +3,18 @@ import PropTypes from 'prop-types';
 import Square from './Square';
 
 const Board = ({ squares, handleClick }) => (
-    <div className="board">
-        <div className="board__row">
+    <div className="game__board">
+        <div className="game__board__row">
             <Square value={squares[0]} handleClick={() => handleClick(0)} />
             <Square value={squares[1]} handleClick={() => handleClick(1)} />
             <Square value={squares[2]} handleClick={() => handleClick(2)} />
         </div>
-        <div className="board__row">
+        <div className="game__board__row">
             <Square value={squares[3]} handleClick={() => handleClick(3)} />
             <Square value={squares[4]} handleClick={() => handleClick(4)} />
             <Square value={squares[5]} handleClick={() => handleClick(5)} />
         </div>
-        <div className="board__row">
+        <div className="game__board__row">
             <Square value={squares[6]} handleClick={() => handleClick(6)} />
             <Square value={squares[7]} handleClick={() => handleClick(7)} />
             <Square value={squares[8]} handleClick={() => handleClick(8)} />
