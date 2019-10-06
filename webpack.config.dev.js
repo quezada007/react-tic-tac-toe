1f1a83007151b81350bea9c80c02085a4af6824d
Jose Quezada <quezada007@yahoo.com>
2019-10-06 08:16:13 -0700

    add browser-sync to the project

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = merge(baseConfig, {
     mode: 'development',
@@ -9,5 +10,25 @@ module.exports = merge(baseConfig, {
         open: true,
         hot: true
     },
-    devtool: 'source-map'
+    devtool: 'source-map',
+    plugins: [
+        new BrowserSyncPlugin(
+          // BrowserSync options
+          {
+            // browse to http://localhost:3000/ during development
+            host: 'localhost',
+            port: 3000,
+            // proxy the Webpack Dev Server endpoint
+            // (which should be serving on http://localhost:8080/)
+            // through BrowserSync
+            proxy: 'http://localhost:8080/'
+          },
+          // plugin options
+          {
+            // prevent BrowserSync from reloading the page
+            // and let Webpack Dev Server take care of this
+            reload: false
+          }
+        )
+    ]
 });
